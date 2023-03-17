5b53987386524c76646b343fd026df2834887ca3
kal13 <lkallee@Abstract-technology.de>
2023-03-17 12:46:44 +0200

    updated profile page

--- src/profile/Header/Header.jsx
+++ src/profile/Header/Header.jsx
@@ -76,7 +76,6 @@ function Header({ courseOrg, courseNumber, courseTitle, intl }) {
           </a>
           <div className="py-2 d-flex align-items-center ">
             {/* {headerLogo} */}
-            
             {/* {authenticatedUser && (
               <AuthenticatedUserDropdown
                 enterpriseLearnerPortalLink={enterpriseLearnerPortalLink}
